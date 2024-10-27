262cbf47166e1e804c3824d5efdf37b8c6cd94de
Luuk <devosluuk@hotmail.nl>
2024-10-27 15:29:50 +0100

    first commit

---
+++ Seniorconnect_Luuk_deVos/Seniorconnect_Luuk_deVos/Model/MySqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Seniorconnect_Luuk_deVos.Model
+{
+    public class MySqlDbContext
+    {
+        private readonly string _connectionString;
+
+        public MySqlDbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
